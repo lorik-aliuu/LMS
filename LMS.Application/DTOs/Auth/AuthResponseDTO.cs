73d35bb15f5ded325dde53243e4d946d5b700164
lorik aliu <loriku123321@gmail.com>
2025-12-06 23:42:10 +0100

    added DTO for returning authentication response

---
+++ LMS.Application/DTOs/Auth/AuthResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.DTOs.Auth
+{
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
