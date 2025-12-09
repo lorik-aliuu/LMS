52bad9cf41da14d1190a5e3af458080ca0913713
lorik aliu <loriku123321@gmail.com>
2025-12-09 17:43:23 +0100

    added validators for changePassword and UpdateUser

---
+++ LMS.Application/Validators/ChangePasswordDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using LMS.Application.DTOs.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.Validators
+{
+    public class ChangePasswordDtoValidator : AbstractValidator<ChangePasswordDTO>
+    {
+        public ChangePasswordDtoValidator()
+        {
+            RuleFor(x => x.CurrentPassword)
+                .NotEmpty().WithMessage("Current password is required");
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty().WithMessage("New password is required")
+                .MinimumLength(6).WithMessage("New password must be at least 6 characters")
+                .Matches(@"[A-Z]").WithMessage("New password must contain at least one uppercase letter")
+                .Matches(@"[a-z]").WithMessage("New password must contain at least one lowercase letter")
+                .Matches(@"[0-9]").WithMessage("New password must contain at least one digit")
+                .NotEqual(x => x.CurrentPassword).WithMessage("New password must be different from current password");
+
+            RuleFor(x => x.ConfirmNewPassword)
+                .NotEmpty().WithMessage("Confirm new password is required")
+                .Equal(x => x.NewPassword).WithMessage("Passwords do not match");
+        }
+    }
+}
---
+++ LMS.Application/Validators/UpdateUserDtoValidator.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using LMS.Application.DTOs.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.Validators
+{
+   
+    public class UpdateUserProfileDtoValidator : AbstractValidator<UpdateUserDTO>
+    {
+        public UpdateUserProfileDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required")
+                .MaximumLength(50).WithMessage("First name cannot exceed 50 characters")
+                .Matches("^[a-zA-Z ]*$").WithMessage("First name can only contain letters and spaces");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required")
+                .MaximumLength(50).WithMessage("Last name cannot exceed 50 characters")
+                .Matches("^[a-zA-Z ]*$").WithMessage("Last name can only contain letters and spaces");
+
+            RuleFor(x => x.PhoneNumber)
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Phone number must be valid")
+                .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+
+            RuleFor(x => x.DateOfBirth)
+                .LessThan(DateTime.Today).WithMessage("Date of birth must be in the past")
+                .GreaterThan(DateTime.Today.AddYears(-120)).WithMessage("Date of birth is too far in the past")
+                .Must(BeAtLeast13YearsOld).WithMessage("You must be at least 13 years old")
+                .When(x => x.DateOfBirth.HasValue);
+
+         
+        }
+
+        private bool BeAtLeast13YearsOld(DateTime? dateOfBirth)
+        {
+            if (!dateOfBirth.HasValue) return true;
+
+            var age = DateTime.Today.Year - dateOfBirth.Value.Year;
+            if (dateOfBirth.Value.Date > DateTime.Today.AddYears(-age)) age--;
+
+            return age >= 13;
+        }
+
+       
+    }
+}
