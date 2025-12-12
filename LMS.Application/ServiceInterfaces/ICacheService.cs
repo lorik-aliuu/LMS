58cc0f7849fd66b8e1a1e1ed9f0dbc2b11e3dbfe
lorik aliu <loriku123321@gmail.com>
2025-12-12 17:06:21 +0100

    added ICacheService for query caching also for rate limiting

---
+++ LMS.Application/ServiceInterfaces/ICacheService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.ServiceInterfaces
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T>(string key) where T : class;
+
+        Task SetAsync<T>(string key, T value, TimeSpan? expiration = null) where T : class;
+
+        Task RemoveAsync(string key);
+
+        Task<bool> ExistsAsync(string key);
+
+        Task<long> IncrementAsync(string key, TimeSpan? expiration = null);
+
+        Task RemoveByPatternAsync(string pattern);
+        Task<long> GetCounterAsync(string key);
+
+
+    }
+}
